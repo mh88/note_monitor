dcb796629db7f7cb7ca8d8510b904ba1559ea3c6
mh88 <mh882008@gmail.com>
2019-07-05 14:36:23 +0800

    Create note_linux.sh

---
+++ note_linux.sh
@@ -0,0 +1,8 @@
+// postgresql 
+su - postgres
+psql
+\l              //list database
+\c cam_config   //change database
+\q              //exit
+\d              //view tables relations
+\d config       //view table config relations
